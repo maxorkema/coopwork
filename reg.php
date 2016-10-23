210073c7ef98498880db8c060f85bb334e5cc096
VANYUHA <bahbok777@gmail.com>
2016-10-23 21:01:24 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -2,37 +2,36 @@
 <html lang="ru">
   <head>
     <meta charset="utf-8">
-    
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Реєстрація</title>
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-      
-      <form>
+      <div class="h1"><img src="undead_n.png" width="150" 
+   height="150" alt="Logo"><h1>Undead</h1><h2>Nation</h2></div>
+      <form class="form-2">
+      <a>Реєстрація</a>
         <section class="container one">
-          <p><label for="user_name">Ім'я</label>
+          <p><label for="user_name">Ім'я            </label>
             <input type="text" name="user_name" pattern="^[А-Яа-яЁё\s]+$"><span></span></p>
           
-          <p><label for="user_surname">Прізвище</label>
+          <p><label for="user_surname">Прізвище  </label>
             <input type="text" name="user_surname" pattern="^[А-Яа-яЁё\s]+$"><span></span></p>
           
-          <p><label for="city">Місто</label>
+          <p><label for="city">Місто        </label>
             <input type="text" name="city" pattern="^[А-Яа-яЁё\s]+$"><span></span></p>
           
-          <p><label for="email">Ел.Пошта</label>
+          <p><label for="email">Ел.Пошта </label>
             <input type="email" name="email"><span></span></p>
           
-          <p><label for="password">Пароль</label>
+          <p><label for="password">Пароль     </label>
             <input type="password" name="password"><span></span></p>
           
           <p><label for="password">Повторіть</label>
             <input type="password" name="password"><span></span></p>
-          
-      
           </p>
-        </section>
-        
+        </section>        
         <section class="container two">
         <button type="submit">Зареєструватися</button>
         </section>
