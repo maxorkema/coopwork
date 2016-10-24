ffe89f41f59cbef628c0b9a59c91e0cb69316a21
Van'ka <Van'ka>
2016-10-24 10:29:58 +0300

    fix after test

--- reg.php
+++ reg.php
@@ -27,7 +27,7 @@
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Реєстрація</title>
+    <title>Реєстрація</title> 
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
@@ -35,29 +35,29 @@
    height="150" alt="Logo"><h1>Undead</h1><h2>Nation</h2></div>
       <form method="post" class="form-2">
       <a>Реєстрація</a>
-        <section class="container one">
+        <div class="container one">
           <p><label for="user_name">Ім'я            </label>
-            <input type="text" name="user_name" pattern="^[А-Яа-яЁёІіЇї\s]+$"><span></span></p>
+            <input type="text" name="user_name" id="user_name" pattern="^[А-Яа-яЁёІіЇї\s]+$"><span></span></p>
           
           <p><label for="user_surname">Прізвище  </label>
-            <input type="text" name="user_surname" pattern="^[А-Яа-яЁёІіЇї\s]+$"><span></span></p>
+            <input type="text" name="user_surname" id="user_surname" pattern="^[А-Яа-яЁёІіЇї\s]+$"><span></span></p>
           
           <p><label for="city">Місто        </label>
-            <input type="text" name="city" pattern="^[А-Яа-яЁёІіЇї\s]+$"><span></span></p>
+            <input type="text" name="city" id="city" pattern="^[А-Яа-яЁёІіЇї\s]+$"><span></span></p>
           
           <p><label for="email">Ел.Пошта </label>
-            <input type="email" name="email"><span></span></p>
+            <input type="email" name="email" id="email"><span></span></p>
           
           <p><label for="password">Пароль     </label>
-            <input type="password" name="password"><span></span></p>
+            <input type="password" name="password" id="password"><span></span></p>
           
           <p><label for="password">Повторіть</label>
-            <input type="password" name="password"><span></span></p>
-          </p>
-        </section>        
-        <section class="container two">
+            <input type="password" name="password" id="passwrod"><span></span></p>
+          
+        </div>        
+        <div class="container two">
         <button type="submit" name="rpassword" id="rpassword">Зареєструватися</button>
-        </section>
+        </div>
       </form>
       
   </body>
